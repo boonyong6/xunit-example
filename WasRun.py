ade11898c6bcdcf3dfdf82df996a3b23e9328df4
boonyong <boonyong.kang93@gmail.com>
2022-11-02 12:09:34 +0800

    Refactor code with setUp()

--- WasRun.py
+++ WasRun.py
@@ -5,11 +5,11 @@ from TestCase import TestCase
 class WasRun(TestCase):
     # Constructor
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
 
     def testMethod(self):
         self.wasRun = 1
 
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
